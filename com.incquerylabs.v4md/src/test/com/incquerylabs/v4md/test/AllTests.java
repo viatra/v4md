5f92089a516149ffec2092cffea03e0efe4bfb82
TheKorpos <gatilacika@gmail.com>
2018-09-20 16:04:02 +0200

    Removed test that fails on purpose

--- com.incquerylabs.v4md/src/test/com/incquerylabs/v4md/test/AllTests.java
+++ com.incquerylabs.v4md/src/test/com/incquerylabs/v4md/test/AllTests.java
@@ -6,7 +6,6 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({
-	ExampleTestCase.class,
-	ExampleFailTest.class
+	ExampleTestCase.class
 })
 public class AllTests {}
