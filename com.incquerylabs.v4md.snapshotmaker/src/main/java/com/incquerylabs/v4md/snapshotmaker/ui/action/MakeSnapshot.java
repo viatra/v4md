fe131229f64d2f7d1f1d48925c160a2f8ba76838
TheKorpos <gatilacika@gmail.com>
2018-09-27 18:06:54 +0200

    Added VQLs to snapshotmaker

--- com.incquerylabs.v4md.snapshotmaker/src/main/java/com/incquerylabs/v4md/snapshotmaker/ui/action/MakeSnapshot.java
+++ com.incquerylabs.v4md.snapshotmaker/src/main/java/com/incquerylabs/v4md/snapshotmaker/ui/action/MakeSnapshot.java
@@ -9,6 +9,7 @@ import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.viatra.query.runtime.api.IQueryGroup;
 import org.eclipse.viatra.query.runtime.api.IQuerySpecification;
 import org.eclipse.viatra.query.runtime.api.ViatraQueryEngine;
 import org.eclipse.viatra.query.runtime.api.ViatraQueryMatcher;
@@ -16,8 +17,9 @@ import org.eclipse.viatra.query.testing.core.SnapshotHelper;
 import org.eclipse.viatra.query.testing.snapshot.QuerySnapshot;
 import org.eclipse.viatra.query.testing.snapshot.SnapshotFactory;
 
+import com.google.common.collect.Sets;
 import com.incquerylabs.v4md.ViatraQueryAdapter;
-import com.incquerylabs.v4md.test.queries.UMLTestQueries;
+import com.incquerylabs.v4md.snapshotmaker.QueryGroups;
 import com.nomagic.magicdraw.actions.MDAction;
 import com.nomagic.magicdraw.core.Application;
 import com.nomagic.magicdraw.core.Project;
@@ -25,7 +27,9 @@ import com.nomagic.magicdraw.core.Project;
 public class MakeSnapshot extends MDAction{
 
 	static final long serialVersionUID = 1L;
-
+	
+	private Set<IQuerySpecification<?>> specsToSave = Sets.newHashSet();
+	private Set<IQueryGroup> groupsToSave = Sets.newHashSet();
 	
 	static String installroot = "";
 	static final String SNAPSHOT_FOLDER = "query_snapshots";
@@ -33,6 +37,9 @@ public class MakeSnapshot extends MDAction{
 	public MakeSnapshot(String id, String name) {
 		super(id, name, null, null);
 		installroot = Application.environment().getInstallRoot();
+		
+		groupsToSave.addAll(QueryGroups.groups());
+		
 	}
 	
 	private void createAndSaveSnapshot(ViatraQueryEngine engine, IQuerySpecification<?> spec) throws IOException{
@@ -41,7 +48,6 @@ public class MakeSnapshot extends MDAction{
 		QuerySnapshot snap = SnapshotFactory.eINSTANCE.createQuerySnapshot();
 
 		helper.saveMatchesToSnapshot((ViatraQueryMatcher)engine.getMatcher(spec), snap);
-		
 		 ResourceSet resourceSet = new ResourceSetImpl();
 		 Resource resource = resourceSet.createResource(URI.createFileURI(new File(installroot + "\\" + SNAPSHOT_FOLDER +"\\" + spec.getSimpleName()+".snapshot").getAbsolutePath()));
 		 resource.getContents().add(snap);
@@ -50,16 +56,32 @@ public class MakeSnapshot extends MDAction{
 			
 	}
 	
+	private void createAndSaveSnapshot(ViatraQueryEngine engine, IQueryGroup group) throws IOException {
+		SnapshotHelper helper = new SnapshotHelper();
+		QuerySnapshot snap = SnapshotFactory.eINSTANCE.createQuerySnapshot();
+		
+		group.getSpecifications().forEach( spec -> helper.saveMatchesToSnapshot((ViatraQueryMatcher)engine.getMatcher(spec), snap));
+		
+		ResourceSet resourceSet = new ResourceSetImpl();
+		Resource resource = resourceSet.createResource(URI.createFileURI(new File(installroot + "\\" + SNAPSHOT_FOLDER +"\\groups\\" + group.getClass().getSimpleName()+".snapshot").getAbsolutePath()));
+		resource.getContents().add(snap);
+		 
+		 resource.save(null);
+		
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent actionEvent) {
 		Project project = Application.getInstance().getProject();
-		ViatraQueryEngine engine = ViatraQueryAdapter.getOrCreateAdapter(project).getEngine();
-		
-		Set<IQuerySpecification<?>> specsToSave = UMLTestQueries.instance().getSpecifications();
+		ViatraQueryEngine engine = ViatraQueryAdapter.getOrCreateAdapter(project).getEngine();	
 		
 		try {
 			for (IQuerySpecification<?> spec: specsToSave) {
 				createAndSaveSnapshot(engine, spec);
+			}
+			
+			for (IQueryGroup g: groupsToSave) {
+				createAndSaveSnapshot(engine, g);
 			}
 			
 			Application.getInstance().getGUILog().showMessage("Snapshots successfully created and saved to: " + installroot + "\\" + SNAPSHOT_FOLDER);
