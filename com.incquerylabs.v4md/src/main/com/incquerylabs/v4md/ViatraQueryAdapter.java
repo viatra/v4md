538cd6280f41411bc0e1275ca7f82ba670262cc1
plunk <lunk.peter@incquerylabs.com>
2018-10-07 17:13:15 +0200

    Manually merged moved VIATRA query adapter

--- com.incquerylabs.v4md/src/main/com/incquerylabs/v4md/ViatraQueryAdapter.java
+++ com.incquerylabs.v4md/src/main/com/incquerylabs/v4md/ViatraQueryAdapter.java
@@ -1,7 +1,14 @@
+
 package com.incquerylabs.v4md;
 
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import org.eclipse.emf.common.notify.Notifier;
 import org.eclipse.emf.common.notify.impl.AdapterImpl;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.viatra.query.runtime.api.AdvancedViatraQueryEngine;
@@ -9,7 +16,6 @@ import org.eclipse.viatra.query.runtime.base.api.BaseIndexOptions;
 import org.eclipse.viatra.query.runtime.emf.EMFScope;
 import org.eclipse.viatra.query.runtime.exception.ViatraQueryException;
 
-import com.google.common.collect.Sets;
 import com.nomagic.magicdraw.core.Project;
 
 public class ViatraQueryAdapter extends AdapterImpl{
@@ -38,27 +44,34 @@ public class ViatraQueryAdapter extends AdapterImpl{
 	}
 	
 	public static Optional<ViatraQueryAdapter> getAdapter(Project project) {
+		Objects.requireNonNull(project, "ViatraQueryAdapter cannot be provided for a null Project");
 		return project.getPrimaryModel().eAdapters().stream().filter(ViatraQueryAdapter.class::isInstance)
 				.map(ViatraQueryAdapter.class::cast).findAny();
 	}
 	
-	public static ViatraQueryAdapter getOrCreateAdapter(Project project) {
+	public static ViatraQueryAdapter getOrCreateAdapter(Project project, Notifier... notifiers) {
 		return getAdapter(project).orElseGet(() -> {
-					ViatraQueryAdapter adapter = null;
-					adapter = new ViatraQueryAdapter(createQueryEngine(project), project);
+					ViatraQueryAdapter adapter = 
+							new ViatraQueryAdapter(
+									createQueryEngine(
+											Stream.concat(project.getModels().stream(), Arrays.stream(notifiers))
+												.collect(Collectors.toSet())), 
+									project);
 					project.getPrimaryModel().eAdapters().add(adapter);
 					return adapter;
 				});
-		
+	}
+	
+	public static ViatraQueryAdapter getOrCreateAdapter(Project project) {
+		return getOrCreateAdapter(project, new Notifier[0]);
 	}
 	
 	public static void disposeAdapter(Project project) {
 		getAdapter(project).ifPresent(ViatraQueryAdapter::dispose);
-		
 	}
 
 	
-	private static AdvancedViatraQueryEngine createQueryEngine(Project project) throws ViatraQueryException {
+	private static AdvancedViatraQueryEngine createQueryEngine(Set<? extends Notifier> setOfRootElement) throws ViatraQueryException {
 		// XXX Omitting references can cause semantic errors (so far we are in the clear though)
 		// these references are only present in UML profiles, typically their contents are equal to the original references inherited from the UML type hierarchy, however there are some cases when this might not be the case.
 		BaseIndexOptions options = new BaseIndexOptions()
@@ -66,6 +79,6 @@ public class ViatraQueryAdapter extends AdapterImpl{
 						&& ((EReference) reference).isContainment() && reference.getName().contains("_from_"))
 				.withStrictNotificationMode(false);
 				
-		return AdvancedViatraQueryEngine.createUnmanagedEngine(new EMFScope(Sets.newHashSet(project.getModels()), options));
+		return AdvancedViatraQueryEngine.createUnmanagedEngine(new EMFScope(setOfRootElement, options));
 	}
 }
