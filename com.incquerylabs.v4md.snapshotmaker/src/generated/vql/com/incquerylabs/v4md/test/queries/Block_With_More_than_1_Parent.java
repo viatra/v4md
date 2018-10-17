35383c8a9adbc816b96f0b1444d8bb85af95f4be
lunkpeter <lunkpeter@hotmail.com>
2018-10-17 19:12:56 +0200

    Updated Snapshots, test project and snapshot maker

---
+++ com.incquerylabs.v4md.snapshotmaker/src/generated/vql/com/incquerylabs/v4md/test/queries/Block_With_More_than_1_Parent.java
@@ -0,0 +1,75 @@
+/**
+ * Generated from platform:/resource/com.incquerylabs.v4md.snapshotmaker/src/main/java/com/incquerylabs/v4md/test/queries/Block_With_More_than_1_Parent.vql
+ */
+package com.incquerylabs.v4md.test.queries;
+
+import com.incquerylabs.v4md.test.queries.BlocksWithDeepGeneralizationHierarchy;
+import com.incquerylabs.v4md.test.queries.GetSuperClasses;
+import com.incquerylabs.v4md.test.queries.MultipleInheritanceBlock;
+import org.eclipse.viatra.query.runtime.api.ViatraQueryEngine;
+import org.eclipse.viatra.query.runtime.api.impl.BaseGeneratedPatternGroup;
+
+/**
+ * A pattern group formed of all public patterns defined in Block_With_More_than_1_Parent.vql.
+ * 
+ * <p>Use the static instance as any {@link interface org.eclipse.viatra.query.runtime.api.IQueryGroup}, to conveniently prepare
+ * a VIATRA Query engine for matching all patterns originally defined in file Block_With_More_than_1_Parent.vql,
+ * in order to achieve better performance than one-by-one on-demand matcher initialization.
+ * 
+ * <p> From package com.incquerylabs.v4md.test.queries, the group contains the definition of the following patterns: <ul>
+ * <li>BlocksWithDeepGeneralizationHierarchy</li>
+ * <li>MultipleInheritanceBlock</li>
+ * <li>getSuperClasses</li>
+ * </ul>
+ * 
+ * @see IPatternGroup
+ * 
+ */
+@SuppressWarnings("all")
+public final class Block_With_More_than_1_Parent extends BaseGeneratedPatternGroup {
+  /**
+   * Access the pattern group.
+   * 
+   * @return the singleton instance of the group
+   * @throws ViatraQueryRuntimeException if there was an error loading the generated code of pattern specifications
+   * 
+   */
+  public static Block_With_More_than_1_Parent instance() {
+    if (INSTANCE == null) {
+        INSTANCE = new Block_With_More_than_1_Parent();
+    }
+    return INSTANCE;
+  }
+  
+  private static Block_With_More_than_1_Parent INSTANCE;
+  
+  private Block_With_More_than_1_Parent() {
+    querySpecifications.add(BlocksWithDeepGeneralizationHierarchy.instance());
+    querySpecifications.add(MultipleInheritanceBlock.instance());
+    querySpecifications.add(GetSuperClasses.instance());
+  }
+  
+  public BlocksWithDeepGeneralizationHierarchy getBlocksWithDeepGeneralizationHierarchy() {
+    return BlocksWithDeepGeneralizationHierarchy.instance();
+  }
+  
+  public BlocksWithDeepGeneralizationHierarchy.Matcher getBlocksWithDeepGeneralizationHierarchy(final ViatraQueryEngine engine) {
+    return BlocksWithDeepGeneralizationHierarchy.Matcher.on(engine);
+  }
+  
+  public MultipleInheritanceBlock getMultipleInheritanceBlock() {
+    return MultipleInheritanceBlock.instance();
+  }
+  
+  public MultipleInheritanceBlock.Matcher getMultipleInheritanceBlock(final ViatraQueryEngine engine) {
+    return MultipleInheritanceBlock.Matcher.on(engine);
+  }
+  
+  public GetSuperClasses getGetSuperClasses() {
+    return GetSuperClasses.instance();
+  }
+  
+  public GetSuperClasses.Matcher getGetSuperClasses(final ViatraQueryEngine engine) {
+    return GetSuperClasses.Matcher.on(engine);
+  }
+}
---
+++ com.incquerylabs.v4md.snapshotmaker/src/generated/vql/com/incquerylabs/v4md/test/queries/Subpattern_Calls.java
@@ -0,0 +1,75 @@
+/**
+ * Generated from platform:/resource/com.incquerylabs.v4md.snapshotmaker/src/main/java/com/incquerylabs/v4md/test/queries/Subpattern_Calls.vql
+ */
+package com.incquerylabs.v4md.test.queries;
+
+import com.incquerylabs.v4md.test.queries.Blocks;
+import com.incquerylabs.v4md.test.queries.BlocksWithNonEmptyName;
+import com.incquerylabs.v4md.test.queries.ClassesWithEmptyName;
+import org.eclipse.viatra.query.runtime.api.ViatraQueryEngine;
+import org.eclipse.viatra.query.runtime.api.impl.BaseGeneratedPatternGroup;
+
+/**
+ * A pattern group formed of all public patterns defined in Subpattern_Calls.vql.
+ * 
+ * <p>Use the static instance as any {@link interface org.eclipse.viatra.query.runtime.api.IQueryGroup}, to conveniently prepare
+ * a VIATRA Query engine for matching all patterns originally defined in file Subpattern_Calls.vql,
+ * in order to achieve better performance than one-by-one on-demand matcher initialization.
+ * 
+ * <p> From package com.incquerylabs.v4md.test.queries, the group contains the definition of the following patterns: <ul>
+ * <li>blocksWithNonEmptyName</li>
+ * <li>classesWithEmptyName</li>
+ * <li>blocks</li>
+ * </ul>
+ * 
+ * @see IPatternGroup
+ * 
+ */
+@SuppressWarnings("all")
+public final class Subpattern_Calls extends BaseGeneratedPatternGroup {
+  /**
+   * Access the pattern group.
+   * 
+   * @return the singleton instance of the group
+   * @throws ViatraQueryRuntimeException if there was an error loading the generated code of pattern specifications
+   * 
+   */
+  public static Subpattern_Calls instance() {
+    if (INSTANCE == null) {
+        INSTANCE = new Subpattern_Calls();
+    }
+    return INSTANCE;
+  }
+  
+  private static Subpattern_Calls INSTANCE;
+  
+  private Subpattern_Calls() {
+    querySpecifications.add(BlocksWithNonEmptyName.instance());
+    querySpecifications.add(ClassesWithEmptyName.instance());
+    querySpecifications.add(Blocks.instance());
+  }
+  
+  public BlocksWithNonEmptyName getBlocksWithNonEmptyName() {
+    return BlocksWithNonEmptyName.instance();
+  }
+  
+  public BlocksWithNonEmptyName.Matcher getBlocksWithNonEmptyName(final ViatraQueryEngine engine) {
+    return BlocksWithNonEmptyName.Matcher.on(engine);
+  }
+  
+  public ClassesWithEmptyName getClassesWithEmptyName() {
+    return ClassesWithEmptyName.instance();
+  }
+  
+  public ClassesWithEmptyName.Matcher getClassesWithEmptyName(final ViatraQueryEngine engine) {
+    return ClassesWithEmptyName.Matcher.on(engine);
+  }
+  
+  public Blocks getBlocks() {
+    return Blocks.instance();
+  }
+  
+  public Blocks.Matcher getBlocks(final ViatraQueryEngine engine) {
+    return Blocks.Matcher.on(engine);
+  }
+}
