1d9b3987b7e3fc7c14b50bb9814e4cc60f7a58e3
Zoltan Ujhelyi <zoltan.ujhelyi@incquerylabs.com>
2019-08-16 18:04:57 +0200

    Experiments with ignoring duplicate removeAdapter events #26

--- com.incquerylabs.v4md/src/main/com/incquerylabs/v4md/internal/MagicDrawProjectNavigationHelper.java
+++ com.incquerylabs.v4md/src/main/com/incquerylabs/v4md/internal/MagicDrawProjectNavigationHelper.java
@@ -14,8 +14,8 @@ import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.viatra.query.runtime.base.api.BaseIndexOptions;
 import org.eclipse.viatra.query.runtime.base.api.filters.IBaseIndexObjectFilter;
 import org.eclipse.viatra.query.runtime.base.api.filters.IBaseIndexResourceFilter;
-import org.eclipse.viatra.query.runtime.base.core.NavigationHelperContentAdapter;
 import org.eclipse.viatra.query.runtime.base.core.NavigationHelperImpl;
+import org.eclipse.viatra.query.runtime.base.core.V4MDNavigationHelperContentAdapter;
 import org.eclipse.viatra.query.runtime.base.exception.ViatraBaseException;
 
 import com.nomagic.uml2.ext.jmi.EventSupport;
@@ -53,20 +53,7 @@ public class MagicDrawProjectNavigationHelper extends NavigationHelperImpl {
 	public MagicDrawProjectNavigationHelper(Notifier emfRoot, BaseIndexOptions options, EventSupport eventSupport, Logger logger) {
 		super(emfRoot, options, new WrappedLogger(logger));
 		this.logger = logger;
-		// TODO While this change helps avoiding some duplicate notifications, it causes
-		// a regression when adding a new project usage to a local project: when the
-		// usage is added, event firing is turned off, resulting in the added profile
-		// not available in the index of the project
-//		this.contentAdapter = new NavigationHelperContentAdapter(this) {
-//			
-//			@Override
-//			public void notifyChanged(Notification notification) {
-//				if (eventSupport.isEnableEventFiring()) {
-//					super.notifyChanged(notification);
-//				}
-//			}
-//			
-//		};
+		this.contentAdapter = new V4MDNavigationHelperContentAdapter(this);
 	}
 	
 	Set<Notifier> getModelRoots() {
